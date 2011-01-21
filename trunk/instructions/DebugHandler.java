7005ec39ccfb2c2ce700e7babfec0d9f828ced47
herrdrjones@gmail.com <herrdrjones@gmail.com@86da9f26-62eb-7a2d-a093-57bd1ee2d322>
2011-01-21 13:54:23 +0000

    the debug instruction

--- trunk/instructions/DebugHandler.java
+++ trunk/instructions/DebugHandler.java
@@ -11,7 +11,35 @@ public class DebugHandler extends InstructionHandler {
 	 * @param instruction The integer value of the instruction to execute, including the four op-code bits.
 	 * @param state The MachineState to use and modify.
 	 */
+	/**
+	 * Total number of registers.
+	 */
+	private static final int REG_COUNT = 8;
+/** 
+ * This method displays the contents of the program counter, registers and the ccr.
+ */
 	@Override
 	public void execute(int instruction, MachineState state, MemoryBank memory) {
+	System.out.println("Progam Counter = 0x" + Integer.toHexString(state.programCounter));
+		int n = 0;
+		int z = 0;
+		int p = 0;
+	
+		for (int i = 0; i < REG_COUNT; i++) 
+		{
+			
+			System.out.println("Register " + i + " = 0x" + Integer.toHexString(state.registers[i]));
+			
+		}
+		if (state.ccrNegative == true) {
+			n = 1;
+		}
+		if (state.ccrZero == true) {
+			z = 1;
+		}
+		if (state.ccrPositive == true) {
+			p = 1;
+		}
+		System.out.println("CCR: N = " + n + " Z = " + z + " P = " + p);
 	}
 }
