b4e987c58cc82865d2b947d89b9826aabc1e8e17
powers.224@buckeyemail.osu.edu <powers.224@buckeyemail.osu.edu@86da9f26-62eb-7a2d-a093-57bd1ee2d322>
2011-02-22 18:04:21 +0000

    Updated test plan with many new cases.

--- trunk/Assembler/tests/test13.asm
+++ trunk/Assembler/tests/test13.asm
@@ -1,4 +1,4 @@
 Test13   .ORIG
 Begin    ADD     R0, R1, #1
-Test     .EQU    Test         ; Should give an error due to the undefined symbol.
+Test     .EQU    Test         ; Should give an error due to aliasing.
          .END    x0
