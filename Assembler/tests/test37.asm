fb8d1db23f0ee0bdb5629fd9098040b1fb303c8d
powers.224@buckeyemail.osu.edu <powers.224@buckeyemail.osu.edu@86da9f26-62eb-7a2d-a093-57bd1ee2d322>
2011-02-22 19:40:13 +0000

    More test cases, TestingPlan revisions.

---
+++ Assembler/tests/test37.asm
@@ -0,0 +1,200 @@
+Test26   .ORIG
+Sym00     .EQU    x3
+Sym01     .EQU    x3
+Sym02     .EQU    x3
+Sym03     .EQU    x3
+Sym04     .EQU    x3
+Sym05     .EQU    x3
+Sym06     .EQU    x3
+Sym07     .EQU    x3
+Sym08     .EQU    x3
+Sym09     .EQU    x3
+Sym10     .EQU    x3
+Sym11     .EQU    x3
+Sym12     .EQU    x3
+Sym13     .EQU    x3
+Sym14     .EQU    x3
+Sym15     .EQU    x3
+Sym16     .EQU    x3
+Sym17     .EQU    x3
+Sym18     .EQU    x3
+Sym19     .EQU    x3
+Sym20     .EQU    x3
+Sym21     .EQU    x3
+Sym22     .EQU    x3
+Sym23     .EQU    x3
+Sym24     .EQU    x3
+Sym25     .EQU    x3
+Sym26     .EQU    x3
+Sym27     .EQU    x3
+Sym28     .EQU    x3
+Sym29     .EQU    x3
+Sym30     .EQU    x3
+Sym31     .EQU    x3
+Sym32     .EQU    x3
+Sym33     .EQU    x3
+Sym34     .EQU    x3
+Sym35     .EQU    x3
+Sym36     .EQU    x3
+Sym37     .EQU    x3
+Sym38     .EQU    x3
+Sym39     .EQU    x3
+Sym40     .EQU    x3
+Sym41     .EQU    x3
+Sym42     .EQU    x3
+Sym43     .EQU    x3
+Sym44     .EQU    x3
+Sym45     .EQU    x3
+Sym46     .EQU    x3
+Sym47     .EQU    x3
+Sym48     .EQU    x3
+Sym49     .EQU    x3
+Sym50     .EQU    x3
+Sym51     .EQU    x3
+Sym52     .EQU    x3
+Sym53     .EQU    x3
+Sym54     .EQU    x3
+Sym55     .EQU    x3
+Sym56     .EQU    x3
+Sym57     .EQU    x3
+Sym58     .EQU    x3
+Sym59     .EQU    x3
+Sym60     .EQU    x3
+Sym61     .EQU    x3
+Sym62     .EQU    x3
+Sym63     .EQU    x3
+Sym64     .EQU    x3
+Sym65     .EQU    x3
+Sym66     .EQU    x3
+Sym67     .EQU    x3
+Sym68     .EQU    x3
+Sym69     .EQU    x3
+Sym70     .EQU    x3
+Sym71     .EQU    x3
+Sym72     .EQU    x3
+Sym73     .EQU    x3
+Sym74     .EQU    x3
+Sym75     .EQU    x3
+Sym76     .EQU    x3
+Sym77     .EQU    x3
+Sym78     .EQU    x3
+Sym79     .EQU    x3
+Sym80     .EQU    x3
+Sym81     .EQU    x3
+Sym82     .EQU    x3
+Sym83     .EQU    x3
+Sym84     .EQU    x3
+Sym85     .EQU    x3
+Sym86     .EQU    x3
+Sym87     .EQU    x3
+Sym88     .EQU    x3
+Sym89     .EQU    x3
+Sym90     .EQU    x3
+Sym91     .EQU    x3
+Sym92     .EQU    x3
+Sym93     .EQU    x3
+Sym94     .EQU    x3
+Sym95     .EQU    x3
+Sym96     .EQU    x3
+Sym97     .EQU    x3
+Sym98     .EQU    x3
+Sym99     .EQU    x3
+Lit00     .EQU    #3
+Lit01     .EQU    #3
+Lit02     .EQU    #3
+Lit03     .EQU    #3
+Lit04     .EQU    #3
+Lit05     .EQU    #3
+Lit06     .EQU    #3
+Lit07     .EQU    #3
+Lit08     .EQU    #3
+Lit09     .EQU    #3
+Lit10     .EQU    #3
+Lit11     .EQU    #3
+Lit12     .EQU    #3
+Lit13     .EQU    #3
+Lit14     .EQU    #3
+Lit15     .EQU    #3
+Lit16     .EQU    #3
+Lit17     .EQU    #3
+Lit18     .EQU    #3
+Lit19     .EQU    #3
+Lit20     .EQU    #3
+Lit21     .EQU    #3
+Lit22     .EQU    #3
+Lit23     .EQU    #3
+Lit24     .EQU    #3
+Lit25     .EQU    #3
+Lit26     .EQU    #3
+Lit27     .EQU    #3
+Lit28     .EQU    #3
+Lit29     .EQU    #3
+Lit30     .EQU    #3
+Lit31     .EQU    #3
+Lit32     .EQU    #3
+Lit33     .EQU    #3
+Lit34     .EQU    #3
+Lit35     .EQU    #3
+Lit36     .EQU    #3
+Lit37     .EQU    #3
+Lit38     .EQU    #3
+Lit39     .EQU    #3
+Lit40     .EQU    #3
+Lit41     .EQU    #3
+Lit42     .EQU    #3
+Lit43     .EQU    #3
+Lit44     .EQU    #3
+Lit45     .EQU    #3
+Lit46     .EQU    #3
+Lit47     .EQU    #3
+Lit48     .EQU    #3
+Lit49     .EQU    #3
+Begin    ADD     R1, R1, Sym00
+         ADD     R1, R1, Sym01
+         ADD     R1, R1, Sym02
+         ADD     R1, R1, Sym03
+         ADD     R1, R1, Sym04
+         ADD     R1, R1, Sym05
+         ADD     R1, R1, Sym06
+         ADD     R1, R1, Sym07
+         ADD     R1, R1, Sym08
+         ADD     R1, R1, Sym09
+         ADD     R1, R1, Sym10
+         ADD     R1, R1, Sym11
+         ADD     R1, R1, Sym12
+         ADD     R1, R1, Sym13
+         ADD     R1, R1, Sym14
+         ADD     R1, R1, Sym15
+         ADD     R1, R1, Sym16
+         ADD     R1, R1, Sym17
+         ADD     R1, R1, Sym18
+         ADD     R1, R1, Sym19
+         ADD     R1, R1, Sym20
+         ADD     R1, R1, Sym21
+         ADD     R1, R1, Sym22
+         ADD     R1, R1, Sym23
+         ADD     R1, R1, Sym24
+         ADD     R1, R1, Sym25
+         ADD     R1, R1, Sym26
+         ADD     R1, R1, Sym27
+         ADD     R1, R1, Sym28
+         ADD     R1, R1, Sym29
+         ADD     R1, R1, Sym30
+         ADD     R1, R1, Lit00
+         ADD     R1, R1, Lit01
+         ADD     R1, R1, Lit02
+         ADD     R1, R1, Lit03
+         ADD     R1, R1, Lit04
+         ADD     R1, R1, Lit05
+         ADD     R1, R1, Lit06
+         ADD     R1, R1, Lit07
+         ADD     R1, R1, Lit08
+         ADD     R1, R1, Lit09
+         ADD     R1, R1, Lit10
+         ADD     R1, R1, Lit11
+         ADD     R1, R1, Lit12
+         ADD     R1, R1, Lit13
+         ADD     R1, R1, Lit14
+         ADD     R1, R1, Lit15
+         ADD     R1, R1, Lit16
+         .END    x0
