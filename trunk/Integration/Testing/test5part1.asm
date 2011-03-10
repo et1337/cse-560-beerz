8270a3af5002c64adb83109ace379e0de7a5e00e
elliotschumacher@gmail.com <elliotschumacher@gmail.com@86da9f26-62eb-7a2d-a093-57bd1ee2d322>
2011-03-10 21:51:27 +0000

    More test cases

---
+++ trunk/Integration/Testing/test5part1.asm
@@ -0,0 +1,34 @@
+;Subroutine for displaying a series of lines of text
+;  The lines of text display a count-down, from X to 0
+;Calling convention: register 3 contains return address
+;Provided by instructor
+;234567890123456789012345678901234567890
+;label___opppp___operandsandcomments...
+;
+Mesg     .ORIG
+         .ENT    Displ,X
+;
+Txt      .STRZ   "Value= "
+X        .FILL   #6
+SavR0    .BLKW   #1
+SavR1    .BLKW   #1
+SavR7    .BLKW   #1
+;
+Displ    ST      R0,SavR0       ;save reg that will be over-written
+         ST      R1,SavR1
+         ST      R7,SavR7
+         LD      R1,X           ;r1 <- M[X]
+         BRN     Done           ;if (r1 < 0) goto Done
+Loop     LEA     R0,Txt
+         TRAP    x22            ;Display text "Value=  "
+         LD      R0,X
+         TRAP    x31            ;Display value in M[X]
+         ADD     R0,R0,#-1
+         ST      R0,X		;M[X] <- r0
+         BRN     Done           ;if (r0 < 0) goto Done
+         JMP     Loop	        ;goto Loop
+Done     LD      R0,SavR0       ;restore registers
+         LD      R1,SavR1
+         LD      R7,SavR7
+         RET
+         .END    Displ
---
+++ trunk/Integration/Testing/test5part2.asm
@@ -0,0 +1,12 @@
+;234567890123456789012345678901234567890
+;label___opppp___operandsandcomments...
+;Provided by instructor
+;
+Data     .ORIG
+         .EXT    X
+         .ENT    V
+V        .FILL   #2
+         TRAP    x43
+Done     TRAP    x25
+         LD      R1,=#1
+         .END    Done
