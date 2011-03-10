c021cf050ced12cb48111976df983192f3d64061
elliotschumacher@gmail.com <elliotschumacher@gmail.com@86da9f26-62eb-7a2d-a093-57bd1ee2d322>
2011-03-10 21:51:27 +0000

    More test cases

---
+++ Integration/Testing/test5part0.asm
@@ -0,0 +1,16 @@
+;Test Program for WI11
+;Provided by instructor
+;234567890123456789012345678901234567890
+;label___opppp___operandsandcomments...
+;
+Main     .ORIG
+         .EXT    Displ,V
+         .ENT    Start
+         .EXT    X
+;
+Start    JSR     Displ   ;Display 6..0
+         LD      R1,V    ;r1 <- M[V]
+         ST      R1,X    ;M[X] <- r1
+         JSR     Displ   ;Display 2..0
+         TRAP    x25     ;halt
+         .END    Start
